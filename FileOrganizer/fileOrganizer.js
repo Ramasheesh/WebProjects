471ca4ef157ffe4417661303a1c636c0bb01b414
RamAsheesh Chauhan <rch231198@gmail.com>
2023-02-19 20:08:14 +0530

    task 2 achived

--- FileOrganizer/fileOrganizer.js
+++ FileOrganizer/fileOrganizer.js
@@ -1,6 +1,18 @@
-//input
-let input =process.argv;
-console.log(input);
-//assumtion
+// //input
+// let input =process.argv;
+// console.log(input);
+// //assumtion
 
-//read folder
+// //read folder
+
+let fs = require('fs');
+let folderPath = process.argv[2];
+// console.log("folderPath");
+let folderExists = fs.existsSync(folderPath);
+if(folderExists){
+    // console.log("path is valid");
+    let files = fs.readdirSync(folderPath);
+    console.log(files);
+}else{
+    console.log("please enter a valid path");
+}
